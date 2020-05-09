2db47f7d09a48bf2578f4822b54149d63b3e254f
Axel Camara <axel.camara@hotmail.fr>
2020-05-10 00:32:35 +0200

    #3 add an option to the cli to allow user to continue from given actor_id

--- python/src/cleaning.py
+++ python/src/cleaning.py
@@ -5,7 +5,7 @@ import json
 
 flatten = lambda l: [item for sublist in l for item in sublist]
 
-def create_actors_dataframe(credits_df, save_path=None):
+def create_actors_dataframe(credits_df, save_path=None, actor_id=None):
     """Create the dataframe of actors present in the tmdb dataset.
     Parameters
     ----------
@@ -21,7 +21,13 @@ def create_actors_dataframe(credits_df, save_path=None):
     """
     columns_to_drop = ['also_known_as']
     actors = flatten([json.loads(item) for index, item in credits_df.cast.iteritems()])
-    list_of_id = set([actor['id'] for actor in actors])
+
+    if actor_id is not None:
+        list_of_id = list(set([actor['id'] for actor in actors]))
+        recover_index = list_of_id.index(actor_id)
+        list_of_id = list_of_id[recover_index:]
+    else:
+        list_of_id = set([actor['id'] for actor in actors])
     actors.clear()
     
     for state, id in enumerate(list_of_id):
@@ -33,10 +39,10 @@ def create_actors_dataframe(credits_df, save_path=None):
             actors.append(actor)
         if save_path is not None and state%500==0:
             actors_df = pd.DataFrame(actors).set_index('id').drop(columns_to_drop, axis=1)
-            actors_df.to_csv(save_path / "actors.csv", index=False)
+            actors_df.to_csv(save_path)
 
     actors_df = pd.DataFrame(actors).set_index('id').drop(columns_to_drop, axis=1)
     if save_path is not None:
-        actors_df.to_csv(save_path / "actors.csv", index=False)
+        actors_df.to_csv(save_path)
     return list_of_id
 
--- python/src/create_dataset.py
+++ python/src/create_dataset.py
@@ -18,9 +18,11 @@ if __name__ == "__main__":
                         help='Choose which dataset you want to create. Just actor is implemented at the moment')
     parser.add_argument('--token', dest='token', type=str,
                         help='Choose which dataset you want to create.')
+    parser.add_argument('--actor_id', dest='actor_id', type=int, default=None,
+                        help='start the download to the given actor id')
     args = parser.parse_args()
     
     if 'actor' in args.dataset:
         tmdb.API_KEY = args.token
         credits = pd.read_csv(data_path / "tmdb-movie-metadata" / "tmdb_5000_credits.csv")
-        _ = create_actors_dataframe(credits, data_path)
+        _ = create_actors_dataframe(credits, data_path / "actors_bis.csv", actor_id=args.actor_id)
