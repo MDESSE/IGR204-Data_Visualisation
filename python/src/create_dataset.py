7adef57653aad5b88838ef8a4970b20203cbe596
Lapidus92 <matthieu.desse@gmail.com>
2020-06-01 18:11:01 +0200

    movie and gender table creation #3

--- python/src/create_dataset.py
+++ python/src/create_dataset.py
@@ -3,19 +3,14 @@ if __name__ == "__main__":
     from pathlib import Path
     import sys
     import pandas as pd
-    #import tmdbsimple as tmdb
+    import tmdbsimple as tmdb
+
+    from cleaning import *
 
     project_path = Path().cwd().parent.parent
     data_path = project_path / "data"
     src_path = project_path / "python" / "src"
     sys.path.append(str(src_path))
-
-    #from cleaning import create_actors_dataframe
-    from cleaning import clean_movies_dataframe
-
-    movies = pd.read_csv(data_path / "tmdb-movie-metadata" / "tmdb_5000_movies.csv")
-    _ = clean_movies_dataframe(movies, data_path / "movies_details.csv")
-
 
     parser = argparse.ArgumentParser()
     parser.add_argument('params', nargs='*', help='list of parameters')
@@ -26,8 +21,16 @@ if __name__ == "__main__":
     parser.add_argument('--actor_id', dest='actor_id', type=int, default=None,
                         help='start the download to the given actor id')
     args = parser.parse_args()
-    
+
     if 'actor' in args.dataset:
         tmdb.API_KEY = args.token
         credits = pd.read_csv(data_path / "tmdb-movie-metadata" / "tmdb_5000_credits.csv")
         _ = create_actors_dataframe(credits, data_path / "actors_bis.csv", actor_id=args.actor_id)
+
+    if 'movie' in args.dataset:
+        movies = pd.read_csv(data_path / "tmdb-movie-metadata" / "tmdb_5000_movies.csv")
+        _ = clean_movies_dataframe(movies, data_path / "movies_details.csv")
+
+    if 'gender' in args.dataset:
+        gender = pd.read_csv(data_path / "tmdb-movie-metadata" / "tmdb_5000_movies.csv")
+        _ = create_genders_table(gender, data_path / "gender.csv")
