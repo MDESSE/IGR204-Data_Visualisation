114acf440a2861c59398dc2006c4a92cb0f39959
Hiroto <yam.hiroto@gmail.com>
2020-06-07 19:36:06 +0200

    axis update issue solved

--- top10/js/top10_movies.js
+++ top10/js/top10_movies.js
@@ -9,18 +9,66 @@ var margin = {top: 10, right: 30, bottom: 40, left: 100},
 
 // append the svg object to the body of the page
 var svg = d3.select("#hiro_dataviz")
-  .append("svg")
+    .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+////////////////////////////////////////////////////////////////////////////////////////////////
 
+// create variables
+var x = d3.scaleLinear()
+.range([ 0, width]);
 
-//tooltip displaying the relevant information when the user hovers or unhovers on an item
+var y = d3.scaleBand()
+.range([ 0, height ])
+.padding(1);
+
+// create labels
+var yaxislabel =svg.append("text")
+.attr("text-anchor", "end")
+.attr("transform", "rotate(0)")
+.attr("y", 0)
+.attr("x", -15);        
+
+var xaxislabel = svg.append("text")
+.attr("text-anchor", "end")
+.attr("x", width)
+.attr("y", height + margin.top + 20);
+    
+// create axis
+var xAxis = svg.append("g")
+.call(d3.axisLeft(x))
+.attr("transform", "translate(0," + height + ")");
+
+var yAxis = svg.append("g")
+.call(d3.axisLeft(y));
+
+// set i to "revenue" (default choice)
+var i = "revenue"
+
+// create mouseover and mouseout functions
+var mouseover = function(d){
+  tooltip.transition()		
+  .duration(200)		
+  .style("opacity", 1);
+
+  tooltip .html("name: " + d.name + "<br/>" + i + " : " + d[i] + "<br/>" + "genre : "+ d.genre)
+  .style("left", (d3.event.pageX + 10) + "px")
+  .style("top", (d3.event.pageY - 15) + "px")
+
+  }
+
+var mouseout = function(d){
+  tooltip.transition()		
+  .duration(200)		
+  .style("opacity", 0)
+}
+
+// create tooltips
 var tooltip = d3.select("body")
-.append("div")  
+    .append("div")  
     .attr("class", "tooltip")
     .style('position','absolute')
     .style("opacity", 0)
@@ -29,282 +77,177 @@ var tooltip = d3.select("body")
     .style("border-width", "1px")
     .style("border-radius", "5px")
     .style("padding", "10px");
+  
 
-    //./data/tmdb-movie-metadata/tmdb_5000_movies.csv
-    d3.csv("/data/tmdb_5000_movies.csv")
-        .row( (d,i) =>{    
-        // Convert quantitative scales to floats
-        
-          try{
-            return {
-              budget: parseFloat(d["budget"]), 
-              revenue: parseFloat(d["revenue"]), 
-              vote_average: parseFloat(d["vote_average"]), 
-              popularity: parseFloat(d["popularity"]),
-              genre: eval(d["genres"])[0]['name'],
-              name: d["original_title"],
-              year: parseInt(d["release_date"].slice(0, 4))
-            };
-          }catch{
-            return {
-              budget: parseFloat(d["budget"]), 
-              revenue: parseFloat(d["revenue"]), 
-              vote_average: parseFloat(d["vote_average"]), 
-              popularity: parseFloat(d["popularity"]),
-              genre: "unknown",
-              name: d["original_title"],
-              year: parseInt(d["release_date"].slice(0, 4))
-            };
-          }
-        })
-    //load the data
-    .get(function(error,rows) {
-        // output some debugging information to the console, so we can make sure everything is loading correctly
-        console.log("Loaded " + rows.length + " rows");
-         if (rows.length >0) {
-             console.log("First row: ", rows[0])
-             console.log("Last row: ", rows[rows.length-1])
-             }
-        
-
-        // //compute the scales when the data is finished loading
-        // x = d3.scaleLinear()
-        // .domain(d3.extent(rows,(row) => row.longitude))
-        // .range([30,w-30]);
-        
-        // y = d3.scaleLinear()
-        // .domain(d3.extent(rows,(row) => row.latitude))
-        // .range([h-30,30]);
-        
-        // r = d3.scaleLinear()
-        // .domain(d3.extent(rows, (row) => row.population))
-        // .range([0, 40])
-
-        // r1 = d3.scaleLinear()
-        // .domain([0,400])
-        // .range(['#a598be', '#381085'])
-
-        
-        //the rows parameter to our get() method has the data, so we just need to save it somewhere accessible from the rest of our progream
-        data = rows
-        
-        
-
-    });
-
-    
-
-    d3.select("#label-option").on("change", change)
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    var topData = data.sort(function(a, b) {
-        return d3.descending(+a.revenue, +b.revenue);
-        }).slice(0, 20);
+ //./data/tmdb-movie-metadata/tmdb_5000_movies.csv
+d3.csv("/data/tmdb_5000_movies.csv").then(function(raw_data) {
+  
+  data = raw_data.map(function(d) { 
+    // Convert quantitative scales to floats
+      try{
+        return {
+          budget: parseFloat(d["budget"]), 
+          revenue: parseFloat(d["revenue"]), 
+          vote_average: parseFloat(d["vote_average"]), 
+          popularity: parseFloat(d["popularity"]),
+          genre: eval(d["genres"])[0]['name'],
+          name: d["original_title"],
+          year: parseInt(d["release_date"].slice(0, 4))
+        };
+      }catch{
+        return {
+          budget: parseFloat(d["budget"]), 
+          revenue: parseFloat(d["revenue"]), 
+          vote_average: parseFloat(d["vote_average"]), 
+          popularity: parseFloat(d["popularity"]),
+          genre: "unknown",
+          name: d["original_title"],
+          year: parseInt(d["release_date"].slice(0, 4))
+        };
+      }
+    })
 
-    var i = "revenue"
+    //initiate graph
+    initialGraph(data);
 
-    draw(topData,i);
-
-
-    function change() {
-        i = "revenue";
-
-        topData = data.sort(function(a, b) {
-        return d3.descending(+a.revenue, +b.revenue);
-        }).slice(0, 20);
-
-        //selectValue = d3.select('select').property('value')
-        
-
-      if (this.selectedIndex == 1){
-        topData = data.sort(function(a, b) {
-        return d3.descending(+a.budget, +b.budget);
-        }).slice(0, 20);
-
-        //selectValue = d3.select('select').property('value')
-        i = "budget";
-        
-        draw(topData,i);
-    
-      } else if (this.selectedIndex == 2){
-        topData = data.sort(function(a, b) {
-            return d3.descending(+a.vote_average, +b.vote_average);
-        }).slice(0, 20);
-
-        //selectValue = d3.select('select').property('value')
-        i = "vote_average";
-
-        draw(topData,i);
-        
-      } else if (this.selectedIndex == 3){
-        topData = data.sort(function(a, b) {
-            return d3.descending(+a.popularity, +b.popularity);
-        }).slice(0, 20);
-        
-        //selectValue = d3.select('select').property('value')
-        i = "popularity";
-        
-
-        
-        draw(topData,i);
-        }
-        
-        console.log(topData);
-        console.log(selectValue);
-    };
-
-    function draw(topData,i) {
-       
-       
-        selectValue = d3.select('select').property('value')
-        
-        d3.select("#label-option").on("change", change)
-
-       
-        // var topData = data.sort(function(a, b) {
-        //      return d3.descending(+a.revenue, +b.revenue);
-        //  }).slice(0, 20);
-
-            // Add X axis
-        var x = d3.scaleLinear()
-        .domain([0, d3.max(topData, function(d) { return d[i] ;} )])
-        .range([ 0, width]);
-
-        svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-20)")
-        .style("text-anchor", "end");
-
-        // Y axis
-        var y = d3.scaleBand()
-        .range([ 0, height ])
-        .domain(topData.map(function(d) { return d.name; }))
-        .padding(1);
-
-        svg.append("g")
-        .call(d3.axisLeft(y))
-
-        svg.selectAll(".bar")
-        .data(topData)
-        .enter()
-        .append("rect")
-        .attr("class", "bar")
-        .attr("x",  function(d) {return  x(0);})
-        .attr("y", function(d) { return y(d.name); })
-        // no bar at the beginning thus:
-        .attr("width",function(d){return x(0);} )
-        .attr("height", 15)
-        .on("mouseover", mouseover )
-        .on("mouseout", mouseout);
-
-        // removed data:
-        svg.selectAll(".bar").exit().remove();
-        svg.selectAll("text").exit().remove();
-
-        var bar = svg.selectAll(".bar")
-        .data(topData, function(d) {
-          return d.name
-        })
+    //update graph based on selection from HTML dragdown
+    d3.select("#label-option").on("change", change);
+  });
 
 
-        // updated data:
-        svg.selectAll(".bar")
-        .transition()
-        .duration(2000)
-        //.attr("x", function(d) { return x(d.revenue); })
-        .attr("y", function(d) { return y(d.name); })
-        .attr("width",function(d) { return  x(d[i]);}  )
-        .attr("height", 15);
+////////////////////////////////////////////////////////////////////////////////////////////////
 
-        //   // ENTER -- ADD ITEMS THAT ARE NEW IN DATA
-        // bar.enter().append("g")
-        // .attr("transform", function(d) {return "translate(" + y(d.name) + ",0)";})
-        // .attr("class", 'bar')
-        // .append("rect")
-        // .attr("x", function(d) {return x(0);})                            //https://stackoverflow.com/questions/45579350/d3-how-to-update-the-chart-after-selection-from-drop-down-menu-with-new-data
-        // .attr("width", function(d) { return  x(d[i]);})
-        // .attr("height",15)  
+// create function initialGraph
+function initialGraph(data){
 
-        // // UPDATE EXISTING ITEMS
-        // svg.selectAll(".bar")
-        // .transition(100)
-        // .attr("transform", function(d) {return "translate(" + y(d.name) + ",0)"})
-        // .select('rect')
-        // .attr("x", function(d) {return x(0);}) 
-        // .attr("height", function(d) { return  x(d[i]);})
-        // .attr("height",15)  
+  selectValue = d3.select('select').property('value')
 
-        // // REMOVE ITEMS DELETED FROM DATA
-        // bar.exit().remove()
+  i = "revenue"
 
-        // // Lines
-        // svg.selectAll("myline")
-        // .data(topData)
-        // .enter()
-        // .append("line")
-        // .attr("x1", x(0))
-        // .attr("x2", x(0))
-        // .attr("y1", function(d) { return y(d.name); })
-        // .attr("y2", function(d) { return y(d.name); })
-        // .attr("stroke", "grey")
+  // select topData based on i
+  var topData = data.sort(function(a, b) {
+    return d3.descending(+a[i], +b[i]);
+    }).slice(0, 20);
 
-        // // Circles -> start at X=0
-        // svg.selectAll("mycircle")
-        // .data(topData)
-        // .enter()
-        // .append("circle")
-        // .attr("cx", x(0) )
-        // .attr("cy", function(d) { return y(d.name); })
-        // .attr("r", "7")
-        // .style("fill", "#69b3a2")
-        // .attr("stroke", "black")
+  // rescale the domain
+  x.domain([0, d3.max(topData, function(d) { return d[i] ;} )]);
+  y.domain(topData.map(function(d) { return d.name; }));
 
-        // // Change the X coordinates of line and circle
-        // svg.selectAll("circle")
-        // .transition()
-        // .duration(2000)
-        // .attr("cx", function(d) { return x(d.revenue); })
+  
+  //initiate X axis
+  xAxis
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+  .attr("transform", "translate(-10,0)rotate(-20)")
+  .style("text-anchor", "end");
 
-        // svg.selectAll("line")
-        // .transition()
-        // .duration(2000)
-        // .attr("x1", function(d) { return x(d.revenue); })
+  //initiate Y axis
+  yAxis
+  .call(d3.axisLeft(y));
 
 
-    };
+  //initiate X axis label
+  xaxislabel.text(i);
 
-    var mouseover = function(d){
+  //initiate Y axis label
+  yaxislabel.text("movies");
 
-        tooltip.transition()		
-            .duration(200)		
-            .style("opacity", 1);
+  //initiate bars, all starting at 0 at the beginning
+  svg.selectAll(".bar")
+  .data(topData)
+  .enter()
+  .append("rect")
+  .attr("class", "bar")
+  .attr("x",  function(d) {return  x(0);})
+  .attr("y", function(d) { return y(d.name); })
+  .attr("width",function(d){return x(0);} )
+  .attr("height", 15)
+  .on("mouseover", mouseover )
+  .on("mouseout", mouseout);
 
-        tooltip .html("name: " + d.name + "<br/>" + i + " : " + d[i])
-            .style("left", (d3.event.pageX + 10) + "px")
-            .style("top", (d3.event.pageY - 15) + "px");
+
+  //update the bar with the transition
+  svg.selectAll(".bar")
+  .transition()
+  .duration(2000)
+  .attr("width",function(d) { return  x(d[i]);}  )
+  .attr("y", function(d) { return y(d.name); })
+  .attr("height", 15);
+
+};
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//create update function 
+
+function change() {
+  selectValue = d3.select('select').property('value');
+
+  if (this.selectedIndex == 0){ 
+    i = "revenue";   
+  } else if (this.selectedIndex == 1){
+    i = "budget";
+  } else if (this.selectedIndex == 2){
+    i = "vote_average";
+  } else if (this.selectedIndex == 3){
+    i = "popularity";
     }
 
-    var mouseout = function(d){
-        
+  
+  //update topData
+  topData = data.sort(function(a, b) {
+    return d3.descending(+a[i], +b[i]);
+  }).slice(0, 20);
+
+
+  // update x and y domain / scale       
+  x
+  .domain([0, d3.max(topData, function(d) { return d[i] ;} )])
+  .call(d3.axisBottom(x));
     
-        tooltip.transition()		
-            .duration(200)		
-            .style("opacity", 0)
-            
-        }
+  y
+  .domain(topData.map(function(d) { return d.name; }))
+  .call(d3.axisLeft(y));
+    
+
+  // Update x axis label   (y axis remains unchanged)
+  xaxislabel
+  .attr("text-anchor", "end")
+  .text(i);
+  
+  
+  // Update the Y-axis and X-axis
+  yAxis
+  .transition()
+  .duration(1500)
+  .call(d3.axisLeft(y));
+  
+  xAxis
+  .transition()
+  .duration(1500)
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+  .attr("transform", "translate(-10,0)rotate(-20)")
+  .style("text-anchor", "end");
 
 
-    //mouseover and mouseout function
+  // Update data:
+  svg.selectAll(".bar")
+  .data(topData)
+  .transition()
+  .duration(2000)
+  .attr("class", "bar")
+  .attr("x",  function(d) {return  x(0);})
+  .attr("y", function(d) { return y(d.name); })
+  .attr("width",function(d){return x(0);} )
+  .attr("width",function(d) { return  x(d[i]);}  )
+  .attr("height", 15);
 
-    //to add tooltip
-    //https://jkeohan.wordpress.com/2015/03/09/using-d3-tooltips/
-    //https://bl.ocks.org/d3noob/a22c42db65eb00d4e369  
-    //https://www.d3-graph-gallery.com/graph/scatter_tooltip.html
 
-    //to add title
-    //http://www.d3noob.org/2013/01/adding-title-to-your-d3js-graph.html
+  }
 
-    // for the html color
-    //https://htmlcolorcodes.com/
+  
+       
