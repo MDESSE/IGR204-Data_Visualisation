6bdce565ef43898db10b37d9c023ba35909f0c9a
Axel Camara <axel.camara@hotmail.fr>
2020-06-16 22:16:12 +0200

    #10 change wordcloud on data update

--- d3/js/filter.js
+++ d3/js/filter.js
@@ -5,6 +5,7 @@
 //import top10 from "./top10_movies.js";
 
 var top10 = require("./top10_movies.js");
+var wordcloud = require("./wordcloud.js");
 
 var d3 = require("d3")
 
@@ -496,6 +497,7 @@ function filterdata(data){
         brush()
       }  
       top10.change(selected)
+      wordcloud.change(selected)
   }
 
   // render a set of polylines on a canvas
